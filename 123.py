16359ec3f9bc4b8f3b12685cf72fcd5f203b9ab0
Jason <jason.auth@gmail.com>
2020-09-22 16:18:25 -0400

    develop commit

--- 123.py
+++ 123.py
@@ -1,25 +1,61 @@
-# Main Game Loop
-# while True:
-#     # Call the class method to display game info
-#     Store.display_game_info()
+from tkinter import *
+from tkinter import ttk
+
+my_window = Tk()
+# frame_name = Frame(my_window)
+
+my_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+           '12', '13', '14', '15', '16', '17', '18', '19', '20']
+my_list2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+            '12', '13', '14', '15', '16', '17', '18', '19', '20']
+
+
+# listbox_object = Listbox(my_window)
+# listbox_object2 = Listbox(my_window)
+# # listbox.pack()
+# listbox_object.grid(row=0, column=1)
+# listbox_object2.grid(row=0, column=3)
 #
-#     # Display Store Info
-#     print("Available options: ")
-#     print("Advance time by: (D)ay, (W)eek, (M)onth")
-#     print("(B)uy Store")
-#     print("(Q)uit")
-#     result = input("Please Enter Your Selection: ")
-#     if result.upper() == "B":
-#         Store.buy_store()
-#     elif result.upper() == "D":
-#         Store.increment_day()
-#     elif result.upper() == "W":
-#         Store.increment_week()
-#     elif result.upper() == "M":
-#         Store.increment_month()
-#     elif result.upper() == "Q":
-#         break
-#     else:
-#         print("Bad Input")
+# scrollbar_object = Scrollbar(my_window)
+# scrollbar_object2 = Scrollbar(my_window)
+# # scrollbar.pack(side=RIGHT, fill=Y)
+# scrollbar_object.grid(row=0, column=2, sticky='ns')
+# scrollbar_object2.grid(row=0, column=4, sticky='ns')
 #
-# print("Thank you for playing Python Idle Tycoon")
+# for item in my_list:
+#     listbox_object.insert(END, item)
+#
+# for item in my_list2:
+#     listbox_object2.insert(END, item)
+#
+# # attach listbox to scrollbar
+# listbox_object.config(yscrollcommand=scrollbar_object.set)
+# listbox_object2.config(yscrollcommand=scrollbar_object2.set)
+# scrollbar_object.config(command=listbox_object.yview)
+# scrollbar_object2.config(command=listbox_object2.yview)
+# mainloop()
+
+
+main_frame = Frame(my_window)
+main_frame.grid(row=0, column=0, sticky="nswe")
+
+left_frame = Frame(main_frame)
+left_frame.grid(row=0, column=0, sticky="nswe")
+
+# Button added just to see that there is a left frame, otherwise it will shrink
+button_object = Button(left_frame, text="My Button")
+button_object.grid(row=0, column=0)
+
+right_frame = Frame(main_frame)
+right_frame.grid(row=0, column=1, sticky="nswe")
+
+listbox_object = Listbox(right_frame)
+listbox_object2 = Listbox(right_frame)
+listbox_object.grid(row=0, column=0)
+listbox_object2.grid(row=0, column=2)
+
+scrollbar_object = Scrollbar(right_frame)
+scrollbar_object2 = Scrollbar(right_frame)
+scrollbar_object.grid(row=0, column=1, sticky='ns')
+scrollbar_object2.grid(row=0, column=3, sticky='ns')
+mainloop()
--- pythonidletycoon.py
+++ pythonidletycoon.py
@@ -1,11 +1,18 @@
 import time
 import tkinter as tk
 from tkinter import ttk
+from tkinter.font import Font
 import csv
+
+root = tk.Tk()
+
+myfont = Font(family="Times New Roman", size=12)
+
 
 FormatMoney = "${:0,.2f}"
 PrintDivider = "---------------------------------------------------------------"
-datafile = "data.csv"
+StoreDataFile = "Storedata.csv"
+timesfont = ("Times New Roman", 11, "bold")
 
 class StoreTimer:
     UpdateFreq = 100
@@ -46,19 +53,23 @@ class Store:
         self.StoreCost = float(storecost)
         self.Timer = float(timer)
         self.ManagerUnlocked = False
-        self.managerCost = float(managercost)
+        self.ManagerCost = float(managercost)
         self.TimerObject = StoreTimer(self)
+        self.clickbutton = None
+        self.progressbar = None
+        self.store_count_label = None
+        self.store_cost_label = None
+        self.buybutton = None
+        self.manager_button = None
 
     @classmethod
     def display_stores(cls):
-        store_label_col1 = tk.Label(root, text="Store Name")
-        store_label_col1.grid(row=4, column=0)
-        store_label_col2 = tk.Label(root, text="Progress")
-        store_label_col2.grid(row=4, column=1)
-        store_label_col3 = tk.Label(root, text="Store Cost")
-        store_label_col3.grid(row=4, column=2)
-        store_label_col3 = tk.Label(root, text="Store Count")
-        store_label_col3.grid(row=4, column=3)
+        tk.Label(root, text="Store Name", font=myfont).grid(row=4, column=0, sticky=tk.W)
+        tk.Label(root, text="Progress", font=myfont).grid(row=4, column=1)
+        tk.Label(root, text="Cost", font=myfont).grid(row=4, column=2, padx=10)
+        tk.Label(root, text="Count", font=myfont).grid(row=4, column=3)
+        tk.Label(root, text="Buy", font=myfont).grid(row=4, column=4)
+        tk.Label(root, text="Unlock", font=myfont).grid(row=4, column=5)
 
         i = 1
         for store in Store.StoreList:
@@ -67,19 +78,21 @@ class Store:
         print(PrintDivider)
 
     def display_store_info(self, i):
-        self.clickbutton = tk.Button(root, text=self.StoreName, command=lambda: self.click_store())
-        self.clickbutton.grid(row=4 + i, column=0)
-        self.progressbar = ttk.Progressbar(root, value=0, maximum=100, orient=tk.HORIZONTAL, length=190,
-                                           mode="indeterminate")
-        self.progressbar.grid(row=4+i, column=1)
+        self.clickbutton = tk.Button(root, text=self.StoreName, width=20, command=lambda: self.click_store())
+        self.clickbutton.grid(row=4 + i, column=0, sticky=tk.W)
+        self.progressbar = ttk.Progressbar(root, value=0, maximum=100, orient=tk.HORIZONTAL
+                                           , length=190, mode="indeterminate")
+        self.progressbar.grid(row=4+i, column=1, padx=(30, 0))
         self.store_cost_label = tk.Label(root, text=FormatMoney.format(self.StoreCost))
-        self.store_cost_label.grid(row=4 + i, column=2)
+        self.store_cost_label.grid(row=4 + i, column=2, sticky=tk.E, padx=20)
         self.store_count_label = tk.Label(root, text=self.StoreCount)
-        self.store_count_label.grid(row=4 + i, column=3)
+        self.store_count_label.grid(row=4 + i, column=3, sticky=tk.E, padx=10)
         self.buybutton = tk.Button(root, text="Buy", command=lambda: self.buy_store())
-        self.buybutton.grid(row=4 + i, column=4)
-        self.manager_button = tk.Button(root, text="Unlock Manager", command=lambda: self.unlock_manager())
+        self.buybutton.grid(row=4 + i, column=4, padx=10)
+        self.manager_button = tk.Button(root, text="Unlock Manager: %s" % FormatMoney.format(self.ManagerCost)
+                                        , width=25, command=lambda: self.unlock_manager())
         self.manager_button.grid(row=4 + i, column=5)
+
 
     def buy_store(self):
         if self.StoreCost <= Store.Money:
@@ -95,8 +108,8 @@ class Store:
             self.manager_button['state'] = tk.DISABLED
 
     def unlock_manager(self):
-        if Store.Money >= self.managerCost and self.ManagerUnlocked is False:
-            Store.Money -= self.managerCost
+        if Store.Money >= self.ManagerCost and self.ManagerUnlocked is False:
+            Store.Money -= self.ManagerCost
             self.ManagerUnlocked = True
             self.TimerObject.start_timer()
             Game.update_ui()
@@ -104,7 +117,8 @@ class Store:
 
 
     def click_store(self):
-        self.TimerObject.start_timer()
+        if self.StoreCount > 0:
+            self.TimerObject.start_timer()
 
     def make_money(self):
         dailyprofit = self.StoreProfit * self.StoreCount
@@ -120,7 +134,7 @@ class GameManager:
 
     def create_stores(self):
         # Read data from file
-        with open(datafile, newline='') as f:
+        with open(StoreDataFile, newline='') as f:
             reader = csv.reader(f)
             for row in reader:
                 Store.StoreList.append(Store(*row))
@@ -131,18 +145,17 @@ class GameManager:
 
     def display_game_header(self):
         root.title("Python Idle Tycoon Business Game")
-        root.geometry("700x300")
-        money_label = tk.Label(root, text="Money")
-        money_label.grid(row=0, column=0)
-        self.dollars_label = tk.Label(root, text=FormatMoney.format(Store.Money))
-        self.dollars_label.grid(row=1, column=0)
+        root.geometry("800x300")
+        self.money_label = tk.Label(root, text=FormatMoney.format(Store.Money), font="Helvetica 15 bold")
+        self.money_label.grid(row=0, column=0, sticky=tk.W)
 
     def update_ui(self):
-        self.dollars_label.config(text=FormatMoney.format(Store.Money))
+        self.money_label.config(text=FormatMoney.format(Store.Money), font="Helvetica 15 bold")
 
 
 # Main Game Loop
-root = tk.Tk()
+
+
 
 Game = GameManager()
 root.mainloop()
